6bd0a57092b329b5817c1210551076d52366ec8a
randy <randy64@bu.edu>
2022-03-11 14:45:57 -0500

    Added tailwind; working on login/register

--- api/api.py
+++ api/api.py
@@ -1,12 +1,16 @@
 import time
-from flask import Flask
+from flask import *
+from flask_pymongo import PyMongo
+from webargs import flaskparser, fields
 import requests
 from datetime import datetime, timezone
 
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/db"
 api_key = "a7e2e1bae88846eeb2c0d6a4bbe63e06"
 
+mongo = PyMongo(app)
 
 @app.route('/joke')
 def get_joke():
@@ -17,3 +21,13 @@ def get_joke():
 @app.route('/time')
 def get_current_time():
     return {'time': datetime.now(timezone.utc).strftime("%Y-%m-%d")}
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request
+    mongo.db["users"].insert_one
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    pass
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,28 @@
+import { React } from "react";
+import  './styles/LoginPage.css';
+import "tailwindcss/tailwind.css"
+function LoginPage(props){
+    return (
+        <>
+        <div>
+            <form class="mb-6" method="post">
+            <div class="flex flex-col mb-4">
+            <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" >
+                Username:
+                <input class="border py-2 px-3 text-grey-darkest" type="text" name="name" />
+            </label>
+            </div>
+            <div class="flex flex-col mb-4">
+            <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" >
+                Password:
+                <input class="border py-2 px-3 text-grey-darkest" type="text password" name="password" />
+            </label>
+            </div>
+            <button class="block bg-teal hover:bg-teal-dark text-white uppercase text-lg mx-auto p-4 rounded" type="submit">Login</button>
+            </form>
+        </div>
+        </>
+    );
+}
+
+export default LoginPage;
