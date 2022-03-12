5a59ee9072d6db4b601fc6c5849723e3b58a32fc
randy <randy64@bu.edu>
2022-03-11 19:17:06 -0500

    added mmallow data validation; interface with mongo

--- api/api.py
+++ api/api.py
@@ -1,11 +1,17 @@
+
 import time
+from json import dumps, loads
+
 from flask import *
 from flask_pymongo import PyMongo
-from webargs import flaskparser, fields, validate
+from hashlib import scrypt
+from os import urandom
+from marshmallow import Schema, fields, ValidationError
 import requests
 from datetime import datetime, timezone
 
 #TODO: Finish implementing register and login functionality
+
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/db"
@@ -13,16 +19,18 @@ api_key = "a7e2e1bae88846eeb2c0d6a4bbe63e06"
 
 mongo = PyMongo(app)
 
-REGISTER_FORM_ARGS = {
-    'Email': fields.Email(required=True),
-    'username': fields.Str(required=True),
-    'password': fields.Str(required=True),
-}
+SALT_LENGTH = 24
 
-LOGIN_FORM_ARGS = {
-    'username': fields.Email(required=True),
-    'password': fields.Str(validate=lambda p: len(p) >= 8),
-}
+class REGISTER_FORM_SCHEMA(Schema):
+    email=fields.Str(required=True)
+    username=fields.Str(required=True)
+    password=fields.Str(required=True)
+
+
+class LOGIN_FORM_SCHEMA(Schema):
+    username=fields.Str(required=True)
+    password=fields.Str(validate=lambda p: len(p) >= 8)
+
 
 @app.route('/joke')
 def get_joke():
@@ -37,11 +45,56 @@ def get_current_time():
 
 @app.route('/register', methods=['POST'])
 def register():
-    args = parser.parse(REGISTER_FORM_ARGS, request)
-    mongo.db["users"].insert_one(args)
-    return 0
+    request_data = request.form
+    register_schema = REGISTER_FORM_SCHEMA()
+    try:
+        result = register_schema.load(request_data)
+    except ValidationError as err:
+        # Return a nice message if validation fails
+        return jsonify(err.messages), 400   
+    if (mongo.db['users'].find_one({'username': result['username']}) is not None):
+        return {'error': 'username already used'}
+    print(result['password'])
+    result['password'], result['salt'] = hashPassword(result['password'])
+    mongo.db["users"].insert_one(result)
+    return redirect(url_for('front_page_data', user=result['username']))
+
+@app.route('/home/<user>')
+def front_page_data(user):
+    return {"ok": user}
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    pass
+    request_data = request.form
+    login_schema = LOGIN_FORM_SCHEMA()
+    try:
+        result = login_schema.load(request_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    db_result = mongo.db['users'].find_one({'username': result['username']})    
+    if (db_result is None):
+        return {'error': 'Username not found'}
+    user = result['username']
+    salt = db_result['salt']
+    password_attempt = result['password']
+    
+    if not checkPassword(user, password_attempt, salt):
+        return {'error':'Password not correct'}
+    return redirect(url_for('front_page_data', user=result['username']))
+    
+      
+
+
+def hashPassword(password: str):
+    iterations = 500_000
+    salt = urandom(SALT_LENGTH)
+    digest = scrypt(password.encode(), salt=salt, n=2, r=8, p=1)
+    return digest.hex(), salt.hex()
+
+def checkPassword(user: str, passwordAttempt: str, salt: str):
+    user_db = mongo.db['users'].find_one({'username': user})
+    hashed_attempt = scrypt(passwordAttempt.encode(), salt=bytes.fromhex(salt), n=2, r=8, p=1).hex()
+    user_password = user_db['password']
+    return user_password == hashed_attempt
+
--- api/mongotest.py
+++ api/mongotest.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient("mongodb://localhost:27017/")
-db=client
+db=client.admin
+
 # Issue the serverStatus command and print the results
 serverStatusResult=db.command("serverStatus")
 pprint(serverStatusResult)
